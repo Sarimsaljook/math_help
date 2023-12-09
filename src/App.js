0908298f7cd98783a7f78ed6837087081a6ef4c3
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2023-12-09 12:01:48 +0500

    Make Login Page UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import LoginPage from "./LoginPage";
-import SignUp from "./SignUp";
+import Home from "./components/Home";
+import LoginPage from "./components/LoginPage";
+import SignUp from "./components/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<Home />}/>
           <Route path="/signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './../styles/LoginPage.css';
+import { useNavigate } from 'react-router-dom';
+
+const LoginPage = () => {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="app">
+      <div className="login-form">
+        <div className="title">Welcome Back!</div> 
+
+        <img
+          src={require('./../assets/mathHelpLogo.png')}
+          width={120}
+          height={120}
+          style={{
+              display: 'block',  // Ensures the image is treated as a block element
+              margin: 'auto',    // Centers the image horizontally
+              }}
+          alt="Math Help Logo"      
+        />
+
+    <div className="form" style={{ marginTop: 40}}>
+     <form>
+       <div className="input-container">
+         <label>Username: </label>
+         <input type="text" name="uname" required />
+        
+       </div>
+       <div className="input-container">
+         <label>Password: </label>
+         <input type="password" name="pass" required />
+       </div>
+
+       <div style={{ display: 'flex', justifyContent: 'center', marginTop: 25 }}>
+        <label style={{ textDecoration: 'underline' }} onClick={() => {
+          navigate('/signup');
+        }}>Not Signed Up?</label>
+       </div>
+
+       <div className="button-container">
+         <button className='login-button'>Login</button>
+       </div>
+      </form>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
