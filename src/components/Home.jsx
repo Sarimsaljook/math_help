5717cdd93eb785dba8e45e2ddffe26a16b757497
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2024-01-09 19:04:22 +0500

    Add Uploaded Papers Display

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar, Form, Button, NavDropdown, Nav, Offcanvas } from 'react-bootstrap';
+import { Container, Navbar, Button, Nav, Offcanvas } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+
+import { ref, listAll } from 'firebase/storage';
+import { ref as databaseRef, onValue } from 'firebase/database';
+
+import { database, storage } from '../firebase';
 
 const Home = () => {
 
   const navigate = useNavigate();
+  const [papers, setPapers] = useState([]);
+
+  const [studentName, setStudentName] = useState('');
+
+  useEffect(() => {
+
+    let isMounted = true; // Variable to track component mount status
+    let processedPapers = 0;
+
+    const fetchData = async () => {
+      try {
+        const storageRef = ref(storage, '/PastPapersStorage');
+
+        const uniqueStudentIds = new Set();
+
+         listAll(storageRef).then((res) => {
+
+           res.items.forEach((item) => {
+            const paperId = item.name;
+            const paperRef = databaseRef(database, `uploadedPapers/${paperId}`);
+
+            onValue(paperRef, (snapshot) => {
+
+              if (!isMounted) return; // Exit if component is unmounted
+
+              const data = snapshot.val();
+
+              setPapers((prevPapers) => [...prevPapers, { id: paperId, data }]);
+
+              try {
+                const userRef = databaseRef(database, `users/${data.student}`);
+        
+                onValue(userRef, (snapshot) => {
+                  const userData = snapshot.val();
+        
+                  if (userData && userData.name) {
+                    setStudentName(userData.name);
+                  } else {}
+                });
+        
+              } catch (error) {
+                console.error('Error fetching student name:', error);
+              }
+           
+
+            });
+
+          });
+        }).catch((err) => console.log(err));
+        
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+      fetchData();
+
+      // Cleanup function to unsubscribe from Firebase event listener
+  return () => {
+    isMounted = false; // Set component as unmounted
+  };
+    }, []);
+
+  const openPdfInNewTab = (pdfUrl) => {
+    window.open(pdfUrl, '_blank');
+  };
+
+  
     
   return (
     
@@ -59,12 +132,40 @@ const Home = () => {
      </div>
      
      <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh', fontSize: 15, textAlign: 'center', width: 800, marginLeft: 350}}> 
-        This is where you can upload and view other students like you who have attmpeted cambirdge IGCSE (0580) math past papers. This will help you gain a better understanding of how a paper is attempted from a students perspective</p>
+        This is where you can upload and view other students like you who have attmpeted cambdge IGCSE (0580) math past papers. This will help you gain a better understanding of how a paper is attempted from a students perspective</p>
 
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  marginBottom: -100, marginTop: 70}}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  marginBottom: -100, marginTop: 40}}>
         <Button variant="success" onClick={() => navigate('/uploadExam')}>Upload Your Attempted Past Papers Here 📝</Button>
   </div>
 
+  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 150 }}>
+        <h2 style={{ color: 'crimson' }}>Uploaded Solved Math Past Papers</h2>
+        <div style={{ overflowY: 'auto', maxHeight: '400px', width: '80%', marginTop: 20 }}>
+          {papers.map((paper) => (
+            <div
+              key={paper.id}
+              style={{
+                border: '1px solid #ccc',
+                borderRadius: '10px',
+                padding: '15px',
+                margin: '10px',
+                cursor: 'pointer',
+                boxShadow: '2px 2px 5px rgba(0, 0, 0, 0.2)',
+              }}
+              onClick={() => openPdfInNewTab(`https://firebasestorage.googleapis.com/v0/b/math-help-ab174.appspot.com/o/PastPapersStorage%2F${paper.id}?alt=media&token=6e477dfe-3b3b-47b8-a98a-bf11e1613722`)}
+            >
+              <p>{`Session: ${
+                paper.data.paperSession === 'M/J' ? 'May/June' :
+                paper.data.paperSession === 'O/N' ? 'October/November' :
+                paper.data.paperSession === 'F/M' ? 'Febuary/March' :
+                'Unknown Session'
+                }, Year: ${paper.data.paperYear}, Variant: ${paper.data.paperVariant}`}</p>
+              <p>{`Solved By: ${studentName}`}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+  
      </div>
   )
 }
